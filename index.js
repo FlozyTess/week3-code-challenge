6b674f84a056abfb34350410835b9f48d7709832
FlozyTess <cecilrency23@gmail.com>
2024-12-15 23:52:19 +0300

    added event listener

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const filmList = document.getElementById('films');
+    const titleElement = document.getElementById('title');
+    const posterElement = document.getElementById('poster');
+    const descriptionElement = document.getElementById('description');
+    const runtimeElement = document.getElementById('runtime');
+    const showtimeElement = document.getElementById('showtime');
+    const ticketsLeftElement = document.getElementById('tickets-left');
+    const buyTicketButton = document.getElementById('buy-ticket');
+    
